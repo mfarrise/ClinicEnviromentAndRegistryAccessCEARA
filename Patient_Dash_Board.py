7baebeb85b24b2b391a6d1f25b8a91b451c420c2
mfarrise <141314687+mfarrise@users.noreply.github.com>
2026-02-06 23:48:55 +0300

    started tinkering with patient dash board and desided i dont building the components continue tweeking

--- Patient_Dash_Board.py
+++ Patient_Dash_Board.py
@@ -2,15 +2,21 @@
 import sys
 
 
-from PySide6.QtWidgets import QWidget, QGridLayout, QPushButton, QFileDialog, QLineEdit, QApplication, QTextEdit, QLabel
+from PySide6.QtWidgets import QWidget, QGridLayout, QPushButton, QFileDialog, QLineEdit, QApplication, QTextEdit, \
+    QLabel, QRadioButton
 
 
 class Patient_Dash_Board(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Patient Dashboard")
+        # self.setMaximumSize(1000, 900)
         Layout = QGridLayout(self)
         self.setLayout(Layout)
+        self.move(0,0)
+        ruler=QLabel(self)
+        Layout.addWidget(ruler,0,0,1,140)
+
 
         patient_name_edit = QLineEdit(self)
         patient_name_edit.setPlaceholderText("Patient Name")
@@ -30,28 +36,67 @@ class Patient_Dash_Board(QWidget):
         # create patient_... variable and assign text to it
         patient_residence_edit.textChanged.connect(lambda text: setattr(self, "patient_residence", text))
 
+        #previous history
+        # label
         previous_history_label = QLabel(self)
         previous_history_label.setText("Previous History")
         Layout.addWidget(previous_history_label,2,1)
 
+        # previous history
+        # text edit
         previous_history_edit = QTextEdit(self)
         previous_history_edit.setReadOnly(True)
-        Layout.addWidget(previous_history_edit, 3, 1,6,10)
+        Layout.addWidget(previous_history_edit, 3, 1,30,20)
 
+        # Last investigations
+        # label
+        last_investigations_label = QLabel(self)
+        last_investigations_label.setText("last investigations")
+        Layout.addWidget(last_investigations_label, 33, 1)
+
+        # last_investigations
+        # text edit
+        last_investigations_edit = QTextEdit(self)
+        last_investigations_edit.setReadOnly(True)
+        Layout.addWidget(last_investigations_edit, 34, 1, 30, 2)
+
+        # Last medications
+        # label
+        last_medications_label = QLabel(self)
+        last_medications_label.setText("last medications")
+        Layout.addWidget(last_medications_label, 33, 4)
+
+        # last medications
+        # text edit
+        last_medications_edit = QTextEdit(self)
+        last_medications_edit.setReadOnly(True)
+        Layout.addWidget(last_medications_edit, 34, 4, 30, 10)
+
+        #Today history
+        #label
         today_history_label = QLabel(self);
         today_history_label.setText("Today History")
-        Layout.addWidget(today_history_label, 9, 1)
+        Layout.addWidget(today_history_label, 2, 21)
+
+        # Today history
+        #Line Edit
         today_history_edit = QTextEdit(self)
         today_history_edit.setReadOnly(False)
-        Layout.addWidget(today_history_edit, 10, 1, 6, 10)
+        Layout.addWidget(today_history_edit, 3, 21, 25, 21)
 
+        #Today Examination
+        #label
         today_examination_label = QLabel(self);
         today_examination_label.setText("Today Examination")
-        Layout.addWidget(today_examination_label, 19, 1)
+        Layout.addWidget(today_examination_label, 28, 21)
+
+        # Today Examination
+        #text edit
         today_examination_edit = QTextEdit(self)
         today_examination_edit.setReadOnly(False)
-        Layout.addWidget(today_examination_edit, 20, 1, 6, 10)
+        Layout.addWidget(today_examination_edit, 29, 21, 6, 20)
 
+    #Just Button
         fetch_path_button = QPushButton(self)
         fetch_path_button.setText("Fetch Path")
         Layout.addWidget(fetch_path_button, 30, 1)
