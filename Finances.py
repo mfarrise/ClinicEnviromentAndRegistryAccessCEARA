be6ae413271307594d171b5a65658f8a363e4611
mohammed from mint <mohammad.aljanabi@gmail.com>
2026-02-18 22:40:19 +0300

    already built the finances data entry section

---
+++ Finances.py
@@ -0,0 +1,123 @@
+import sys
+
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QWidget, QGridLayout, QLabel, QApplication, QLineEdit, QPushButton, QSizePolicy
+
+
+#ill also creat sub layouts that sit inside the finances_layout for the sake of sanity of my mind
+class Finances(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Finance")
+        self.finances_layout = QGridLayout()
+        self.setLayout(self.finances_layout)
+
+        #################         data input section     ###############
+        #region
+
+        self.data_input_section=QWidget()
+        self.data_input_layout = QGridLayout()
+        self.data_input_section.setLayout(self.data_input_layout)
+
+        self.date_label = QLabel("Dates")
+        self.date_label.setAlignment(Qt.AlignCenter)
+        self.data_input_layout.addWidget(self.date_label, 0, 1)
+
+        self.entries_label = QLabel("Entries")
+        self.entries_label.setAlignment(Qt.AlignCenter)
+        self.data_input_layout.addWidget(self.entries_label, 0, 2)
+
+
+
+        self.balance_label = QLabel("balance")
+        self.data_input_layout.addWidget(self.balance_label, 1, 0)
+
+        self.date_balance_line_edit = QLineEdit()
+        self.date_balance_line_edit.setPlaceholderText("Date")
+        self.data_input_layout.addWidget(self.date_balance_line_edit, 1, 1)
+
+        self.balance_line_edit = QLineEdit()
+        self.balance_line_edit.setPlaceholderText("insert income")
+        self.data_input_layout.addWidget(self.balance_line_edit, 1, 2)
+
+        self.balance_date_today_button = QPushButton("Today")
+        self.data_input_layout.addWidget(self.balance_date_today_button, 1, 3)
+        self.balance_date_today_button.setFocusPolicy(Qt.ClickFocus)
+        #self.balance_date_today_button.clicked.connect()
+
+        self.balance_push_button = QPushButton("Push")
+        self.data_input_layout.addWidget(self.balance_push_button, 1, 4)
+        self.balance_push_button.setFocusPolicy(Qt.ClickFocus)
+        # self.balance_push_button.clicked.connect()
+
+
+
+        self.patient_no_label = QLabel("patient No.")
+        self.data_input_layout.addWidget(self.patient_no_label, 2, 0)
+
+        self.p_no_date_line_edit = QLineEdit()
+        self.p_no_date_line_edit.setPlaceholderText("Date")
+        self.data_input_layout.addWidget(self.p_no_date_line_edit, 2, 1)
+
+        self.p_no_line_edit = QLineEdit()
+        self.p_no_line_edit.setPlaceholderText("insert number")
+        self.data_input_layout.addWidget(self.p_no_line_edit, 2, 2)
+
+        self.p_no_date_today_button = QPushButton("Today")
+        self.data_input_layout.addWidget(self.p_no_date_today_button, 2, 3)
+        self.p_no_date_today_button.setFocusPolicy(Qt.ClickFocus)
+        #self.p_no_date_today_button.clicked.connect()
+
+        self.p_no_push_button = QPushButton("Push")
+        self.data_input_layout.addWidget(self.p_no_push_button, 2, 4)
+        self.p_no_push_button.setFocusPolicy(Qt.ClickFocus)
+        # self.bp_no_push_button.clicked.connect()
+
+
+
+
+        self.note_label = QLabel("Note")
+        self.data_input_layout.addWidget(self.note_label, 3, 0)
+
+        self.note_date_line_edit = QLineEdit()
+        self.note_date_line_edit.setPlaceholderText("Date")
+        self.data_input_layout.addWidget(self.note_date_line_edit, 3, 1)
+
+        self.note_line_edit = QLineEdit()
+        self.note_line_edit.setPlaceholderText("insert note")
+        self.data_input_layout.addWidget(self.note_line_edit, 3, 2)
+
+        self.note_date_today_button = QPushButton("Today")
+        self.data_input_layout.addWidget(self.note_date_today_button, 3, 3)
+        self.note_date_today_button.setFocusPolicy(Qt.ClickFocus)
+        #self.note_date_today_button.clicked.connect()
+
+        self.note_push_button = QPushButton("Push")
+        self.data_input_layout.addWidget(self.note_push_button, 3, 4)
+        self.note_push_button.setFocusPolicy(Qt.ClickFocus)
+        # self.note_push_button.clicked.connect()
+
+        self.all_date_today_button = QPushButton("Today")
+        self.data_input_layout.addWidget(self.all_date_today_button, 4, 1,1,3)
+        #self.all_date_today_button.clicked.connect()
+
+        self.all_push_button = QPushButton("Push")
+        self.data_input_layout.addWidget(self.all_push_button, 5, 1,1,3)
+        # self.all_push_button.clicked.connect()
+
+
+        self.finances_layout.addWidget(self.data_input_section,0,0)#change position later when other sublayout are added
+        self.data_input_section.setSizePolicy(
+            QSizePolicy.Fixed,
+            QSizePolicy.Fixed
+        )
+        #endregion
+
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = Finances()
+    window.show()
+    sys.exit(app.exec())
--- Main.py
+++ Main.py
@@ -4,11 +4,12 @@ from PySide6.QtGui import QGuiApplication
 from eGFRSubWindow import eGFRSubWindow
 from ContrastRiskSubWindow import ContrastRiskSubWindow
 from PatientDashBoard import PatientDashBoard
-
+from Finances import Finances
 
 class MainWindow_class(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.screen_geometry = QGuiApplication.primaryScreen().availableGeometry()
         self.setWindowTitle("C.E.A.R.A. v0.3")
         central_widget = QWidget()
         layout = QVBoxLayout()
@@ -22,7 +23,7 @@ class MainWindow_class(QMainWindow):
         gfr_button=QPushButton("GFR")
         gfr_button.clicked.connect(self.call_gfr)
         finances_button=QPushButton("Finances")
-        #finances_button.clicked.connect(self.call_finances)
+        finances_button.clicked.connect(self.call_finances)
 
         layout.addWidget(standard_button)
         layout.addWidget(dash_board_button)
@@ -34,9 +35,17 @@ class MainWindow_class(QMainWindow):
         self.eGFRWindow = eGFRSubWindow()
         self.CINWindow = ContrastRiskSubWindow()
         self.PatientWindow = PatientDashBoard()
+        self.financesWindow = Finances()
+
         self.screen_size=QGuiApplication.primaryScreen().availableGeometry()#availablesize() ignores task bar geometry account for task bar height
         self.screen_width=self.screen_size.width()
         self.screen_height=self.screen_size.height()
+
+        self.show()
+
+        window_geometry = self.frameGeometry()
+        window_geometry.moveCenter(self.screen_geometry.center())
+        self.move(window_geometry.topLeft())
 
 
         # self.resize(self.screen_width,self.screen_height)
@@ -46,26 +55,37 @@ class MainWindow_class(QMainWindow):
         self.call_cin()
     def call_gfr(self):
         #calling egfr calculator window
-        self.eGFRWindow.adjustSize()
+
         self.eGFRWindow.show()
+        frame_rect = self.eGFRWindow.frameGeometry()
+        frame_rect.moveCenter(self.screen_geometry.topRight())
+        self.eGFRWindow.move(frame_rect.topLeft())
         self.eGFRWindow.raise_()
 
     def call_cin(self):
 
-
         self.CINWindow.show()
+        frame_rect=self.CINWindow.frameGeometry()
+        frame_rect.moveCenter(self.screen_geometry.bottomRight())
+        self.CINWindow.move(frame_rect.topLeft())
+
         self.CINWindow.raise_()
-        self.CINWindow.adjustSize()
+
 
     def call_patient_dashboard(self):
         self.PatientWindow.show()
         self.PatientWindow.raise_()
 
-
+    def call_finances(self):
+        self.financesWindow.show()
+        frame_rect = self.financesWindow.frameGeometry()
+        frame_rect.moveCenter(self.screen_geometry.center())
+        self.financesWindow.move(frame_rect.topLeft())
+        self.financesWindow.raise_()
 
 App = QApplication(sys.argv)
 MainWindow = MainWindow_class()
-MainWindow.setWindowTitle("C.E.A.R.A. v0.3")
+MainWindow.setWindowTitle("C.E.A.R.A. v0.5")
 
 
 
@@ -74,5 +94,5 @@ MainWindow.setWindowTitle("C.E.A.R.A. v0.3")
                                                 #   Final Excution    #
                                                 #######################
 
-MainWindow.show()
+
 App.exec()
