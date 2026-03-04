7407886c273ef0df903b6639d253dd8dba11a679
mohammed from mint <mohammad.aljanabi@gmail.com>
2026-03-04 18:04:59 +0300

    almost finishing the intelliscence tasks remained to do 1) finish symptoms intellisense 2) finish db upload and design 3) deploy to work after testing 4) work on report generation and graphs

---
+++ note.py
@@ -0,0 +1,47 @@
+"""
+Pillars of work flow
+1) finish intelligence of symptoms
+2) finsih the demographics data
+3) start designing the db according to the dashboard
+4) implement visit (first ,ongoing,locked) logic
+5) try extensivly
+6) consider docx appending
+7) START designing and implementing report and graph generation
+
+
+
+
+Main.py
+########
+
+
+
+CIN.py
+#######
+
+eGFR.py
+########
+
+DashBoard.py
+############
+    demographics
+        add patient phone number
+        find a way to incorporate the kind of job where labour retired or desk
+
+
+
+
+
+Finances
+########
+
+
+
+
+
+
+
+
+
+
+"""
